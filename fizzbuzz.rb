2bac9a840fae477ed266a2d7675a6fe6db88ac1f
Danny Tseng <dtfocus1025@gmail.com>
2019-04-05 02:24:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,5 +4,11 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
-  elsif num % 5 ==
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    nil
+  end
 end
